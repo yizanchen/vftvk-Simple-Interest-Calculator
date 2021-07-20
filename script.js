ad2b358d8316a2b1d3c331aa57e7cce1808877c7
yizanchen <31357578+yizanchen@users.noreply.github.com>
2021-07-19 16:19:58 -1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 function compute()
 {
-
     var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
     if( principal <= 0 ){
         alert("Enter a positive number")
         document.getElementById("principal").focus();
     }
     else{
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    var result = document.getElementById("result").innerHTML;
-
-    result = 
-    "If you deposit <mark>"+principal+
+    document.getElementById("result").innerHTML = 
+    "\<br\>If you deposit <mark>"+principal+
     "</mark>,\<br\>at an interest rate of <mark>"+rate+
     "%</mark>\<br\>You will receive an amount of <mark>"+interest+
-    "</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    "</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
     }
 }
       
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
